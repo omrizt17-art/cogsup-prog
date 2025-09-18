99b1dc9b1479091bb9643bad8496c5a1a4ee6cfc
Omri Zalait <omri.zt17@gmail.com>
2025-09-18 19:02:23 +0200

    Solved Week-1 Exercise 3 (sum, product, squares, etc.)

--- Week-1/Exercises/Exercise-3.py
+++ Week-1/Exercises/Exercise-3.py
@@ -16,7 +16,7 @@ Write code that prints the sum of the elements in the following list.
 """
 
 lst = [1, 4, -6, 7, 2, 3, 9, 11, 6] # In all exercises in this script, you will work with this list
-
+print(sum(lst))
 print("Exercise 3.1")
 
 pass
@@ -30,6 +30,10 @@ Task:
 ------
 Print the product of the elements in the list.
 """
+product = 1
+for x in lst:
+    product *= x
+print(product)
 
 print("Exercise 3.2")
 
@@ -44,7 +48,12 @@ Task:
 ------
 Print the sum of the squares of the list.
 """
-
+square = 1
+sum = 0
+for x in lst:
+    square=x*x
+    sum+=square
+print(sum)
 print("Exercise 3.3")
 
 pass
@@ -58,7 +67,11 @@ Task:
 ------
 Print the largest element of the list.
 """
-
+max1 = 0
+for x in lst:
+    if x>max1:
+        max1 = x
+print(max1)
 print("Exercise 3.4")
 
 pass
@@ -72,7 +85,11 @@ Task:
 ------
 Print the largest element of the list.
 """
-
+max1 = 0
+for x in lst:
+    if x>max1:
+        max1 = x
+print(max1)
 print("Exercise 3.5")
 
 pass
